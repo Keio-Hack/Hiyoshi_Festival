6e11bb57dd2c355b53bdfd2ddc10a6b4ee474485
Takahara Shotaro <Shotaro@TakaharaShotaro.local>
2015-09-30 00:19:07 +0900

    This is a real HP but I do not finish it yet

--- css/main.js
+++ css/main.js
@@ -6,12 +6,45 @@ $(function(){
 	date = today.getDate();
 
 	if(month === 9){
+
 		var count = 31 + 30 - date;
-		// console.log(count);
 		$("#count_down p").append(count);
+
 	}else if(month === 10){
+
 		var count = 31 - date;
 		$("#count_down p").append(count);
+
 	}
+
+
+	$(".time_table").each(function(){
+		var $window = $(window),
+			$header = $(this).find(".title"),
+			$back = $(this).find(".place"),
+			header_offset_top = $header.offset().top - 50,
+			header_offset_bottom =header_offset_top + 90 ;
+
+
+			$window.on("scroll",function(){
+
+				if($window.scrollTop() > header_offset_top){
+					// $header.addClass("sticky");
+					$header.css({
+						position:"fixed",
+						top:"50px"
+					});
+					// $(".time").find("#first").addClass("sti_now");
+				}else if($window.scrollTop() > header_offset_bottom - 60){
+					// $header.removeClass("sticky");
+					$header.css({opacity:0});
+					// $(".time").find("#first").removeClass("sti_now");
+				}else{
+					$header.css({position:"absolute"});
+					$header.css({top:0});
+				}
+
+			});
+	});
 
 });
