9c04d4ce8d876393bd06e3b516eba0d342cde669
taka-sho <takasho530000@gmail.com>
2015-10-19 07:51:13 +0900

    Get two movies from Takasho

--- js/main.js
+++ js/main.js
@@ -1,50 +1,56 @@
 $(function(){
 
-	var today = new Date();
+	function get_date(){
+		var today = new Date();
 
-	month = today.getMonth() + 1;
-	date = today.getDate();
+		var month = today.getMonth() + 1,
+		date = today.getDate();
 
-	if(month === 9){
+		if(month === 9){
 
-		var count = 31 + 30 - date;
-		$("#count_down p").append(count);
+			// var count = 31 + 30 - date;
+			// $("#count_down .inner p").append(count);
 
-	}else if(month === 10){
+		}else if(month === 10){
 
-		var count = 31 - date;
-		$("#count_down p").append(count);
+			var count = 31 - date;
+			// alert(count);
+			$("#count_down p").append(count);
+			count = "";
 
+		}
 	}
 
-
-	$(".time_table").each(function(){
-		var $window = $(window),
-			$header = $(this).find(".title"),
-			$back = $(this).find(".place"),
-			header_offset_top = $header.offset().top - 50,
-			header_offset_bottom =header_offset_top + 90 ;
+	get_date();
 
 
-			$window.on("scroll",function(){
-
-				if($window.scrollTop() > header_offset_top){
-					// $header.addClass("sticky");
-					$header.css({
-						position:"fixed",
-						top:"50px"
-					});
-					// $(".time").find("#first").addClass("sti_now");
-				}else if($window.scrollTop() > header_offset_bottom - 60){
-					// $header.removeClass("sticky");
-					$header.css({opacity:0});
-					// $(".time").find("#first").removeClass("sti_now");
-				}else{
-					$header.css({position:"absolute"});
-					$header.css({top:0});
-				}
-
-			});
-	});
-
+// 	$(".time_table").each(function(){
+// 		var $window = $(window),
+// 			$header = $(this).find(".title"),
+// 			$back = $(this).find(".place"),
+// 			header_offset_top = $header.offset().top - 50,
+// 			header_offset_bottom =header_offset_top + 90 ;
+//
+//
+// 			$window.on("scroll",function(){
+//
+// 				if($window.scrollTop() > header_offset_top){
+// 					// $header.addClass("sticky");
+// 					$header.css({
+// 						position:"fixed",
+// 						top:"50px"
+// 					});
+// 					// $(".time").find("#first").addClass("sti_now");
+// 				}else if($window.scrollTop() > header_offset_bottom - 60){
+// 					// $header.removeClass("sticky");
+// 					$header.css({opacity:0});
+// 					// $(".time").find("#first").removeClass("sti_now");
+// 				}else{
+// 					$header.css({position:"absolute"});
+// 					$header.css({top:0});
+// 				}
+//
+// 			});
+// 	});
+//
 });
